2f1af6df56f2859dabbe98161eacc92e57518dbc
Irtaza Ahmed <66567115+irtaza714@users.noreply.github.com>
2023-07-03 04:25:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def predict_datapoint():
         return render_template('home.html', results=results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80)
